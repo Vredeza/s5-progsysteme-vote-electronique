748c54fe634eb0e8822ef6a858dcaf8ecdd2ee8e
sanchezj <53372645+Vredeza@users.noreply.github.com>
2023-12-21 12:34:36 +0100

    Ajout des handlers pour toute les commandes

--- code/src/serveur_handlers.c
+++ code/src/serveur_handlers.c
@@ -4,6 +4,7 @@
 
 #include "../common/include/messages.h"
 #include "../common/include/serveur_handlers.h"
+#include "../common/include/bd.h"
 #include "stdio.h"
 
 int NOPHandler(Commande* commande){
@@ -36,6 +37,46 @@ int majElecteurHandler(Commande* commande){
     return 0;
 }
 
+int Handler(Commande* commande){
+    printf("Je suis le handler XXXXXX, j'ai reçu une commande de signature %s", commande->signature);
+    return 0;
+}
+
+int ajoutElectionHandler(Commande* commande){
+    printf("Je suis le handler ajoutElectionHandler, j'ai reçu une commande de signature %s", commande->signature);
+    return 0;
+}
+
+int suppressionElectionHandler(Commande* commande){
+    printf("Je suis le handler suppressionElectionHandler, j'ai reçu une commande de signature %s", commande->signature);
+    return 0;
+}
+
+int lectureElectionHandler(Commande* commande){
+    printf("Je suis le handler lectureElectionHandler, j'ai reçu une commande de signature %s", commande->signature);
+    return 0;
+}
+
+int listeElectionHandler(Commande* commande){
+    printf("Je suis le handler listeElectionHandler, j'ai reçu une commande de signature %s", commande->signature);
+    return 0;
+}
+
+int majElectionHandler(Commande* commande){
+    printf("Je suis le handler majElectionHandler, j'ai reçu une commande de signature %s", commande->signature);
+    return 0;
+}
+
+int resultatElectionHandler(Commande* commande){
+    printf("Je suis le handler resultatElectionHandler, j'ai reçu une commande de signature %s", commande->signature);
+    return 0;
+}
+
+int ajoutVoteHandler(Commande* commande){
+    printf("Je suis le handler ajoutVoteHandler, j'ai reçu une commande de signature %s", commande->signature);
+    return 0;
+}
+
 int handler(Commande* commande){
     switch (commande->type) {
         case NOP: return NOPHandler(commande);
@@ -49,6 +90,20 @@ int handler(Commande* commande){
         case LISTE_ELECTEUR: return listeElecteurHandler(commande);
             break;
         case METTRE_A_JOUR_ELECTEUR: return majElecteurHandler(commande);
+            break;
+        case AJOUT_ELECTION: return ajoutElectionHandler(commande);
+            break;
+        case SUPPRIME_ELECTION: return suppressionElectionHandler(commande);
+            break;
+        case LIRE_ELECTION: return lectureElectionHandler(commande);
+            break;
+        case LISTE_ELECTION: return listeElectionHandler(commande);
+            break;
+        case METTRE_A_JOUR_ELECTION: return majElectionHandler(commande);
+            break;
+        case RESULTAT_ELECTION: return resultatElectionHandler(commande);
+            break;
+        case AJOUT_VOTE: return ajoutVoteHandler(commande);
             break;
         default:
             printf("Commande non-reconnue");
